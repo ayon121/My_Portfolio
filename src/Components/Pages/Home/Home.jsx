256299c74f69043dfa045fb272f1deb962a4473f
ayon121 <borshonsaha1234@gmail.com>
2023-12-11 13:45:22 +0600

    first commit

---
+++ src/Components/Pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+import Navbar from "../Shared/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+           <Navbar></Navbar>
+        </div>
+    );
+};
+
+Home.propTypes = {
+    
+};
+
+export default Home;
---
+++ src/Components/Pages/Shared/Navbar.jsx
@@ -0,0 +1,47 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const navlinks = <>
+        <li><NavLink to="/" className={({ isActive }) => isActive ? 'text-sm font-bold text-[#FF444A]' : 'text-sm bg-transparent text-[#f8f5f5]'}>Profile</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? 'text-sm  font-bold text-[#FF444A]' : 'text-sm  bg-transparent text-[#f8f5f5]'} to="/projects" >My Projects</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? 'text-sm  font-bold text-[#FF444A]' : 'text-sm  bg-transparent text-[#f8f5f5]'} to="/About Me" >About Me</NavLink></li>
+
+
+
+    </>
+    return (
+        <div className="bg-[#231f20] font-poppins ">
+            <div className=" navbar max-w-7xl  mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navlinks}
+                        </ul>
+                    </div>
+                    {/* logo */}
+                    <h1 className="text-white text-xl md:text-2xl border-y-2 rounded-md py-1"><span className="text-3xl text-[#FF444A]">A</span><span className="">yon Saha</span></h1>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navlinks}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn btn-xs sm:btn-sm md:btn-md ">Contact Me</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+
+};
+
+export default Navbar;
