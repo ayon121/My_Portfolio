b373260b632bdcc86cc68c4af743ef2fa6b4c420
ayon121 <borshonsaha1234@gmail.com>
2023-12-12 01:24:08 +0600

    service updated

--- src/Components/Pages/Home/MyServices/MyService.jsx
+++ src/Components/Pages/Home/MyServices/MyService.jsx
@@ -1,11 +1,13 @@
 
 import PropTypes from 'prop-types';
+import { LuHeartHandshake } from "react-icons/lu";
 // import { Cursor, useTypewriter } from 'react-simple-typewriter';
 const MyService = ({ service, details }) => {
-  
+
     return (
         <div className="card w-auto bg-[#231f20] font-poppins  shadow-xl border-2 border-[#FF444A]">
             <div className="card-body text-center">
+                <h1 className='text-4xl md:text-5xl text-[#FF444A] text-center flex justify-center'><LuHeartHandshake /></h1>
                 <h2 className=" text-2xl text-[#FF444A]">{service}</h2>
                 <p className='text-white'>{details}</p>
             </div>
