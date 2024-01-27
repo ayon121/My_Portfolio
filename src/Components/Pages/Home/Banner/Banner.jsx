01d1ee8dc07e749a7a166190a34280d92a19aefb
ayon121 <borshonsaha1234@gmail.com>
2024-01-27 17:15:19 +0600

    my picture updated

--- src/Components/Pages/Home/Banner/Banner.jsx
+++ src/Components/Pages/Home/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 // import PropTypes from 'prop-types';
 import "../Banner/Banner.css"
-import mypic from "../../../../assets/images/mypic2.jpg"
+import mypic from "../../../../assets/images/Ayon.gif"
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 const Banner = () => {
     const [text] = useTypewriter({
@@ -10,7 +10,7 @@ const Banner = () => {
       })
     return (
         <div className="featuredimage bg-fixed mb-5  h-auto font-poppins">
-            <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center">
+            <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center pt-4 pb-3">
                 <div className="px-4">
                     <h1 className="text-2xl md:text-4xl lg:text-5xl text-white py-2 ">Ayon Saha</h1>
                     <h2 className="text-[#FF444A] text-sm md:text-lg">{text}<Cursor cursorColor='red' /></h2>
@@ -19,7 +19,7 @@ const Banner = () => {
                     
                 </div>
                 <div>
-                    <img className="rounded-full h-3/6" src={mypic} alt="profile img" />
+                    <img className="rounded-full h-3/6  opacity-85 border-x-8 border-[#FF444A]" src={mypic} alt="profile img" />
                 </div>
             </div>
         </div>
