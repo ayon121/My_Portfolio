9a28a1262d2405c454498edfd155de4585a0eca9
ayon121 <borshonsaha1234@gmail.com>
2024-04-09 03:15:44 -0700

    ttech i know updated

--- src/Components/Pages/Home/TeachIKnow/TeachIKnow.jsx
+++ src/Components/Pages/Home/TeachIKnow/TeachIKnow.jsx
@@ -12,30 +12,30 @@ const TeachIKnow = () => {
     return (
         <div> 
             <Title title={'Technology I Use'}></Title>
-            <div className="border-4 border-black gap-5 px-4 py-3 grid grid-cols-2 md:grid-cols-4 max-w-6xl mx-auto">
-                <div className=" border-4 border-black w-auto flex justify-center" >
-                    <img className="h-28"  src={htmlpic} alt="" />
+            <div className=" gap-9 px-5 md:px-10 py-3 grid grid-cols-2 md:grid-cols-4 max-w-6xl mx-auto">
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl w-auto flex justify-center items-center mx-4 my-2  " >
+                    <img className="h-28 py-2 "  src={htmlpic} alt="" />
                 </div>
-                <div className="border-4 border-black w-auto flex justify-center">
-                    <img className="h-24" src={csspic} alt="" />
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl  w-auto flex justify-center mx-4 my-2 ">
+                    <img className="h-24 " src={csspic} alt="" />
                 </div>
-                <div className="border-4 border-black w-auto flex justify-center">
-                    <img className="h-24" src={tailwindpic} alt="" />
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl  w-auto flex justify-center mx-4 my-2">
+                    <img className="h-24  " src={tailwindpic} alt="" />
                 </div>
-                <div className="border-4 border-black w-auto flex justify-center">
-                    <img className="h-24" src={jspic} alt="" />
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl  w-auto flex justify-center mx-4 my-2">
+                    <img className="h-24  " src={jspic} alt="" />
                 </div>
-                <div className="border-4 border-black w-auto flex justify-center">
-                    <img className="h-24" src={reactpic} alt="" />
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl  w-auto flex justify-center mx-4 my-2">
+                    <img className="h-24  " src={reactpic} alt="" />
                 </div>
-                <div className="border-4 border-black w-auto flex justify-center">
-                    <img className="h-24" src={nodepic} alt="" />
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl  w-auto flex justify-center mx-4 my-2">
+                    <img className="h-24  " src={nodepic} alt="" />
                 </div>
-                <div className="border-4 border-black w-auto flex justify-center" >
-                    <img className="h-24" src={nextpic} alt="" />
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl  w-auto flex justify-center mx-4 my-2" >
+                    <img className="h-24  " src={nextpic} alt="" />
                 </div>
-                <div className="border-4 border-black w-auto flex justify-center">
-                    <img className="h-24" src={mongopic} alt="" />
+                <div className="border-2 border-red-400 shadow-2xl rounded-3xl  w-auto flex justify-center mx-4 my-2">
+                    <img className="h-24  " src={mongopic} alt="" />
                 </div>
             </div>
 
